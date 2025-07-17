4fcb8d3a8e10f9fcccd69ee61a951fe1235c328b
zxvxl <1101012296@qq.com>
2025-07-17 21:11:03 +0800

    Fix feeder service order checks

--- backend/pet-feeder-backend/src/service-orders/service-orders.module.ts
+++ backend/pet-feeder-backend/src/service-orders/service-orders.module.ts
@@ -3,10 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ServiceOrdersService } from './service-orders.service';
 import { ServiceOrdersController } from './service-orders.controller';
 import { ServiceOrder } from './entities/service-order.entity';
+import { Feeder } from '../feeders/entities/feeder.entity';
+import { Order } from '../orders/entities/order.entity';
 import { TrackingModule } from '../tracking/tracking.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ServiceOrder]), TrackingModule],
+  imports: [
+    TypeOrmModule.forFeature([ServiceOrder, Feeder, Order]),
+    TrackingModule,
+  ],
   controllers: [ServiceOrdersController],
   providers: [ServiceOrdersService],
 })
